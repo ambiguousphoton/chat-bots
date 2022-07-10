d7d54061c5cd6596201ec5d70a338d5a4bd4947e
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-10 17:51:53 +0530

    Add files via upload

---
+++ vidyut.py
@@ -0,0 +1,21 @@
+name = "VIDYUT"
+import time
+nowT = time.ctime()
+ver = 1.0
+verAge = 0
+
+qna = {
+    "hi":"hey",
+    "how are you": "I am fine",
+    "what is your name" :  name ,
+    "how old are you" :  verAge ,
+    "what is your version":  ver ,
+    "what is the time now ?": nowT,
+}
+
+while True:
+    qs = input()
+    if(qs == "quit"):
+        break
+    else:
+        print(qna[qs])
