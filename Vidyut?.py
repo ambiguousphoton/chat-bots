2715b38641e27be62d106abf683b32c86a4e7cfd
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-18 22:22:38 +0530

    Update Vidyut?.py

--- Vidyut?.py
+++ Vidyut?.py
@@ -1,2 +1,24 @@
 import string
 import metaplot
+
+name = "VIDYUT"
+import time
+nowT = time.ctime()
+ver = 1.0
+verAge = 0
+
+qna = {
+    "hi":"hey",
+    "how are you": "I am fine",
+    "what is your name" :  name ,
+    "how old are you" :  verAge ,
+    "what is your version":  ver ,
+    "what is the time now ?": nowT,
+}
+
+while True:
+    qs = input()
+    if(qs == "quit"):
+        break
+    else:
+        print(qna[qs])
