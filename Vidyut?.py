45738d5b61f098703f3a2cfc9cefb8ecda002564
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-18 22:26:10 +0530

    Update Vidyut?.py

--- Vidyut?.py
+++ Vidyut?.py
@@ -22,3 +22,17 @@ while True:
         break
     else:
         print(qna[qs])
+
+root = Tk()
+root.title("clock")
+def time():
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text = string)
+    lbl.after(1000, time)
+
+lbl = Label(root, font = ('Times new roman',40,'bold') , background = 'purple' , foreground ='white')
+
+lbl.pack(anchor ='center')
+time()
+
+mainloop()
